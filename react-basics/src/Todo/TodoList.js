b02751d1e64194274ab03edde8bf826f307838fa
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-02-18 11:33:00 +0200

    Todo

---
+++ react-basics/src/Todo/TodoList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+const styles = {
+  ul: {
+    listStyle: "none",
+    margine: 0,
+    padding: 0
+  }
+};
+export default function TodoList() {
+  return (
+    <ul style={styles.ul}>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </ul>
+  );
+}
