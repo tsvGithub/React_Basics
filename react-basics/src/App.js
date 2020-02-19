67cb6b5f9edb90dd1df571d421a807ee8ef7ec5d
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-02-19 10:51:35 +0200

    Todo before change

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,11 +1,47 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
+import { func } from "prop-types";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: "Buy Bread" },
+    { id: 2, completed: true, title: "Buy Butter" },
+    { id: 3, completed: false, title: "Buy Milk" }
+  ]);
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false
+        }
+      ])
+    );
+  }
   return (
-    <div className="wrapper">
-      <h1>React tutorial</h1>
-      <TodoList />
-    </div>
+    <Context.Provider value={{ removeTodo: removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}></AddTodo>
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos!</p>}{" "}
+      </div>
+    </Context.Provider>
   );
 }
 
